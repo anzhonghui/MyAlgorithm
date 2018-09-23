c12da3cc27f63502fb15a94e1e35fc0fef53b223
huihui <1719120364@qq.com>
2018-09-23 10:51:25 +0800

    13. Roman to Integer 14. Integer to Roman

--- LeetCode/src/com/qk/myleetcode/easy/RomanToInteger.java
+++ LeetCode/src/com/qk/myleetcode/easy/RomanToInteger.java
@@ -1,5 +1,8 @@
 package com.qk.myleetcode.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 public class RomanToInteger {
@@ -9,19 +12,65 @@ public class RomanToInteger {
 //		System.out.println(romanToInt("III"));
 //		System.out.println(romanToInt("IV"));
 //		System.out.println(romanToInt("IX"));
-//		System.out.println(romanToInt("LVIII"));
-//		System.out.println(romanToInt("MCMXCIV"));
+		System.out.println(romanToInt("LVIII"));
+		System.out.println(romanToInt("MCMXCIV"));
 //		System.out.println(romanToInt(""));
-		System.out.println(romanToInt("MCDLXXVI"));
+//		System.out.println(romanToInt("MCDLXXVI"));//MDCLXXVI
 	}
-
+	
 	/**
-	 * 方法1，使用hash算法
-	 * 
+	 * 思路：利用map集合的方式
 	 * @param s
 	 * @return
 	 */
 	public int romanToInt(String s) {
+		if (s == null || s.length() == 0 || "".equals(s)) {
+			return 0;
+		}
+		
+		Map<String, Integer> maps = new HashMap<>(13);
+		maps.put("I", 1);
+		maps.put("IV", 4);
+		maps.put("V", 5);
+		maps.put("IX", 9);
+		maps.put("X", 10);
+		maps.put("XL", 40);
+		maps.put("L", 50);
+		maps.put("XC", 90);
+		maps.put("C", 100);
+		maps.put("CD", 400);
+		maps.put("D", 500);
+		maps.put("CM", 900);
+		maps.put("M", 1000);
+		
+		int result = 0;
+		int len = s.length();
+		
+		for (int i = 0; i < len; i++) {
+			
+			if(i + 1 < len) {
+				String temp = s.charAt(i) + "" + s.charAt(i+1);
+				Integer value = maps.get(temp);
+				if(value != null) {
+					result += value;
+					i++;
+					continue;
+				}
+			}
+			
+			result += maps.get(s.charAt(i) + "");
+		}
+		
+		return result;
+	}
+
+	/**
+	 * 方法1，使用hash算法（未成功 - 原来理解的是从后往前统计）
+	 * 
+	 * @param s
+	 * @return
+	 */
+	public int romanToInt1(String s) {
 		if (s == null || s.length() == 0 || "".equals(s)) {
 			return 0;
 		}
---
+++ LeetCode/src/com/qk/myleetcode/medium/IntegerToRoman.java
@@ -0,0 +1,168 @@
+package com.qk.myleetcode.medium;
+
+import org.junit.Test;
+
+public class IntegerToRoman {
+
+	@Test
+	public void MyTest() {
+//		System.out.println(intToRoman(3000));
+//		System.out.println(intToRoman(300));
+//		System.out.println(intToRoman(400));
+//		System.out.println(intToRoman(900));
+//		System.out.println(intToRoman(800));
+//		System.out.println(intToRoman(3300));
+//		System.out.println(intToRoman(3400));
+//		System.out.println(intToRoman(3900));
+//		System.out.println(intToRoman(3800));
+//		System.out.println(intToRoman(90));
+//		System.out.println(intToRoman(80));
+//		System.out.println(intToRoman(40));
+//		System.out.println(intToRoman(30));
+//		System.out.println(intToRoman(9));
+//		System.out.println(intToRoman(8));
+//		System.out.println(intToRoman(4));
+//		System.out.println(intToRoman(3));
+		
+		// leetcode test
+		System.out.println(intToRoman(3));
+		System.out.println(intToRoman(4));
+		System.out.println(intToRoman(9));
+		System.out.println(intToRoman(58));
+		System.out.println(intToRoman(1994));
+		System.out.println(intToRoman(1676));
+	}
+	
+    private static final int[] INTEGERS = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] ROMANS = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    
+    /**
+     * 思路：参考别人的思路，通过提前提取出各种情况，然后通过循环遍历（代码简洁，比较容易理解）
+     * @param num
+     * @return
+     */
+    public String intToRoman(int num) {
+        if (num <= 0) {
+            return "";
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < INTEGERS.length; i++) {
+            while (num >= INTEGERS[i]) {
+                sb.append(ROMANS[i]);
+                num -= INTEGERS[i];
+            }
+        }
+        
+        return sb.toString();
+    }
+
+	/**
+	 * 思路：自己的思路，通过判断各种情况（优点：写起来简单，易于理解；缺点：代码冗余）
+	 * @param num
+	 * @return
+	 */
+	public String intToRomanByMe(int num) {
+
+		StringBuilder sBuilder = new StringBuilder();
+
+		int temp = num;
+
+		while (temp != 0) {
+			// 处理1000的情况
+			if (temp / 1000 != 0) {
+				for (int i = 0; i < temp / 1000; i++) {
+					sBuilder.append('M');
+				}
+				temp %= 1000;
+			}
+
+			// 处理百的情况（两种400 900）
+			int hundred = temp / 100;
+			if (hundred != 0) {
+				// 900
+				if (hundred == 9) {
+					sBuilder.append("CM");
+				}
+				// 500-900
+				if (hundred >= 5 && hundred < 9) {
+					sBuilder.append('D');
+					for (int i = 0; i < hundred - 5; i++) {
+						sBuilder.append('C');
+					}
+				}
+				// 400
+				if (hundred == 4) {
+					sBuilder.append("CD");
+				}
+				if (hundred > 0 && hundred < 4) {
+					for (int i = 0; i < hundred; i++) {
+						sBuilder.append('C');
+					}
+				}
+
+				temp %= 100;
+			}
+			
+			// 处理<100的情况
+			int lessHundred = temp / 10;
+			if(lessHundred != 0) {
+				// 90
+				if(lessHundred == 9) {
+					sBuilder.append("XC");
+				}
+				
+				// >= 50 && < 90
+				if(lessHundred >= 5 && lessHundred < 9) {
+					sBuilder.append('L');
+					for (int i = 0; i < lessHundred - 5; i++) {
+						sBuilder.append('X');
+					}
+				}
+				// 40
+				if(lessHundred == 4) {
+					sBuilder.append("XL");
+				}
+				
+				// >10 && <40
+				if(lessHundred >0 && lessHundred < 4) {
+					for (int i = 0; i < lessHundred; i++) {
+						sBuilder.append('X');
+					}
+				}
+				
+				temp %= 10;
+			}
+			
+			if(temp != 0) {
+				// 9
+				if(temp == 9) {
+					sBuilder.append("IX");
+				}
+				
+				// >= 5 && < 9
+				if(temp >= 5 && temp < 9) {
+					sBuilder.append('V');
+					for (int i = 0; i < temp - 5; i++) {
+						sBuilder.append('I');
+					}
+				}
+				// 4
+				if(temp == 4) {
+					sBuilder.append("IV");
+				}
+				
+				// >1 && <4
+				if(temp >0 && temp < 4) {
+					for (int i = 0; i < temp; i++) {
+						sBuilder.append('I');
+					}
+				}
+				
+				temp = 0;
+			}
+		}
+
+		return sBuilder.toString();
+	}
+}
